8ed078b326658306d10d85222f69bd86ac5d1762
Jostyck9 <hugo.berthome@epitech.eu>
2019-12-14 22:36:00 +0100

    Adding entities and their initialisation

---
+++ cmake-build-debug/CMakeFiles/GravitySimulator.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hugo/repo/github/GravitySimulator/src/Entity.cpp" "/home/hugo/repo/github/GravitySimulator/cmake-build-debug/CMakeFiles/GravitySimulator.dir/src/Entity.cpp.o"
+  "/home/hugo/repo/github/GravitySimulator/src/RenderSystem.cpp" "/home/hugo/repo/github/GravitySimulator/cmake-build-debug/CMakeFiles/GravitySimulator.dir/src/RenderSystem.cpp.o"
+  "/home/hugo/repo/github/GravitySimulator/src/main.cpp" "/home/hugo/repo/github/GravitySimulator/cmake-build-debug/CMakeFiles/GravitySimulator.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/hugo/.conan/data/boost/1.71.0/conan/stable/package/eeafe3fe5902cd9d31cbe358f832100c45afd0a8/include"
+  "/home/hugo/.conan/data/sfml/2.5.1/bincrafters/stable/package/7998827d52d82af547087a0e5998de509c33b987/include"
+  "/home/hugo/.conan/data/freetype/2.9.0/bincrafters/stable/package/3cdd93cd46fc07735d1b0d7f2860a82fac7a3053/include"
+  "/home/hugo/.conan/data/freetype/2.9.0/bincrafters/stable/package/3cdd93cd46fc07735d1b0d7f2860a82fac7a3053/include/freetype2"
+  "/home/hugo/.conan/data/stb/20180214/conan/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/hugo/.conan/data/openal/1.19.0/bincrafters/stable/package/812e1deb664b70de9456fc54c225f826fbca36e7/include"
+  "/home/hugo/.conan/data/openal/1.19.0/bincrafters/stable/package/812e1deb664b70de9456fc54c225f826fbca36e7/include/AL"
+  "/home/hugo/.conan/data/flac/1.3.2/bincrafters/stable/package/525236c0b9904b537adc9952b8c52e9686f35118/include"
+  "/home/hugo/.conan/data/vorbis/1.3.6/bincrafters/stable/package/8d8cebdeda6ff46ba515968aa8b099a24769ca16/include"
+  "/home/hugo/.conan/data/bzip2/1.0.8/conan/stable/package/b27e2b40bcb70be2e7d6f0e0c6e43e35c530f8d3/include"
+  "/home/hugo/.conan/data/libpng/1.6.34/bincrafters/stable/package/b781af3f476d0aa5070a0a35b544db7a3c193cc8/include"
+  "/home/hugo/.conan/data/libalsa/1.1.5/conan/stable/package/bff9a7d5ab267a535181ec5b36392417340ff035/include"
+  "/home/hugo/.conan/data/ogg/1.3.3/bincrafters/stable/package/d50a0d523d98c15bb147b18fa7d203887c38be8b/include"
+  "/home/hugo/.conan/data/zlib/1.2.11/conan/stable/package/d50a0d523d98c15bb147b18fa7d203887c38be8b/include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/GravitySimulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GravitySimulator.dir/src/Entity.cpp.o"
+  "CMakeFiles/GravitySimulator.dir/src/RenderSystem.cpp.o"
+  "CMakeFiles/GravitySimulator.dir/src/main.cpp.o"
+  "bin/GravitySimulator"
+  "bin/GravitySimulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GravitySimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
